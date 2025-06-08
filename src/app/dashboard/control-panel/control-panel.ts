ebd8cf664f00ed09b80816a8c01b971627e03eb3
Melissa Chuqui <smchuqui@sudamericano.edu.ec>
2025-06-08 13:01:08 -0500

    splash-screen_panel-control

--- src/app/dashboard/control-panel/control-panel.ts
+++ src/app/dashboard/control-panel/control-panel.ts
@@ -2,10 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-control-panel',
-  imports: [],
   templateUrl: './control-panel.html',
-  styleUrl: './control-panel.css'
+  styleUrls: ['./control-panel.css']
 })
-export class ControlPanel {
-
-}
+export class ControlPanelComponent {}
--- src/app/splash-screen/splash-screen.ts
+++ src/app/splash-screen/splash-screen.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import {
+  trigger, transition, style, animate, query, stagger, state
+} from '@angular/animations';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-splash-screen',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './splash-screen.html',
   styleUrl: './splash-screen.css'
 })
-export class SplashScreen {
+export class SplashScreenComponent {
 
 }
