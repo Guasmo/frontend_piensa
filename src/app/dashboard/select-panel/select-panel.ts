b305e112a01597826a9af5694ca0f5d59bcf807f
guasmo <juancalveti4@gmail.com>
2025-06-08 16:37:22 -0500

    added select-panel

---
+++ src/app/dashboard/select-panel/select-panel.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-select-panel',
+  imports: [],
+  templateUrl: './select-panel.html',
+  styleUrl: './select-panel.css'
+})
+export class SelectPanel {
+
+}
