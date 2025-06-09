05156d1acad688ca27db436f02e0bd4807d35937
guasmo <juancalveti4@gmail.com>
2025-06-09 01:54:52 -0500

    frontend.v1 finished

--- src/app/dashboard/select-panel/select-panel.ts
+++ src/app/dashboard/select-panel/select-panel.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-select-panel',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './select-panel.html',
   styleUrl: './select-panel.css'
 })
 export class SelectPanel {
+  changeColor(event: MouseEvent, color: string): void {
+    const target = event.target as HTMLElement;
+    target.style.backgroundColor = color;
+
+    // Activar animación brillante
+    target.classList.add('shiny');
+    setTimeout(() => {
+      target.classList.remove('shiny');
+    }, 600);
+  }
 
 }
