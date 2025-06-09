ef9c47ef99786d78893ffcd1922eb5653062d6a7
guasmo <juancalveti4@gmail.com>
2025-06-09 02:39:46 -0500

    test register to backend

--- src/app/auth/register/register.ts
+++ src/app/auth/register/register.ts
@@ -1,12 +1,50 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
-  imports: [RouterLink],
   templateUrl: './register.html',
-  styleUrl: './register.css'
+  styleUrls: ['./register.css']
 })
-export class Register {
+export class RegisterComponent {
+  username: string = '';
+  email: string = '';
+  password: string = '';
+  errorMessage: string = '';
+  successMessage: string = '';
 
+  constructor(private http: HttpClient, private router: Router) {}
+
+  onSubmit() {
+    // Validaciones básicas
+    if (!this.username || !this.email || !this.password) {
+      this.errorMessage = 'Todos los campos son obligatorios.';
+      return;
+    }
+
+    if (this.password.length < 4 || this.password.length > 12) {
+      this.errorMessage = 'La contraseña debe tener entre 4 y 12 caracteres.';
+      return;
+    }
+
+    const payload = {
+      username: this.username,
+      email: this.email,
+      password: this.password
+    };
+
+    this.http.post('https://backendpiensa-production-3d53.up.railway.app//auth/register', payload)
+      .subscribe({
+        next: response => {
+          this.successMessage = '¡Cuenta creada exitosamente!';
+          this.errorMessage = '';
+          setTimeout(() => this.router.navigate(['/auth/login']), 2000);
+        },
+        error: error => {
+          this.successMessage = '';
+          this.errorMessage = error.error?.message || 'Error al registrar.';
+        }
+      });
+  }
 }
